29622a2b82b7a6f9e378df8e43702f989798bb39
Daniel Mihov <daniel.mihovv@gmail.com>
2024-08-01 14:12:01 +0300

    Language config for BG and EN languages with properties plus login utils.

---
+++ src/main/java/com/example/medappointmentscheduler/utils/Utils.java
@@ -0,0 +1,24 @@
+package com.example.medappointmentscheduler.utils;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Utils {
+
+    public boolean isLoggedIn() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth != null && auth.isAuthenticated() && !auth.getPrincipal().equals("anonymousUser");
+    }
+
+    public boolean hasRole(String role) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        if (auth == null) {
+            return false;
+        }
+
+        return auth.getAuthorities().stream().anyMatch(authority -> authority.getAuthority().equals(role.toUpperCase()));
+    }
+}
